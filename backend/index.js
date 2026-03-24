db3cc72e8c192485f0878e1d29ffaa652e93913f
Subbu0306 <subbubandi0306@gmail.com>
2026-03-24 22:34:10 +1030

    Refactor index.js for improved readability

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', DELETE, 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 

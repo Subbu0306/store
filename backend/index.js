99b6eb16623da72af51b4d5d723f4b6b920089cd
Subbu0306 <subbubandi0306@gmail.com>
2026-03-24 18:46:43 +1030

    Update server port and clean up code

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use("/employees", require("./routes/employees")); // Add this line
 //const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 

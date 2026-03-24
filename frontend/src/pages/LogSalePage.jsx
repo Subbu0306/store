f2c77209fd0c1502de389515eb2d5fcb07f24368
Subbu0306 <subbubandi0306@gmail.com>
2026-03-25 00:16:12 +1030

    Update LogSalePage.jsx

--- frontend/src/pages/LogSalePage.jsx
+++ frontend/src/pages/LogSalePage.jsx
@@ -242,7 +242,7 @@ useEffect(() => {
 
     try {
       const token = localStorage.getItem("token");
-      await axios.post(`${import.meta.env.VITE_API_URL}/products`, saleData, {
+      await axios.post(`${import.meta.env.VITE_API_URL}/sale`, saleData, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
